bb585fb958bf601038c152c5afdbcc02f7af4de0
Dreyf <cesardreyf@gmail.com>
2025-11-30 12:34:15 -0300

    Emplea Filesystem en ConfigLoader para las operaciones con archivos

--- bin/runph.php
+++ bin/runph.php
@@ -7,6 +7,7 @@ use Psr\Container\ContainerInterface;
 use Runph\Services\CommandsAutoloader;
 use Runph\Services\Config\ConfigLoader;
 use Runph\Services\Container\Container;
+use Runph\Services\Filesystem\Filesystem;
 use Symfony\Component\Console\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -14,7 +15,14 @@ require __DIR__ . '/../vendor/autoload.php';
 // Settings
 $container = new Container();
 $container->set(ContainerInterface::class, $container);
-$container->set(ConfigLoader::class, new ConfigLoader(dirname(__DIR__) . '/config'));
+
+$container->set(
+    ConfigLoader::class, 
+    new ConfigLoader(
+        $container->get(Filesystem::class),
+        dirname(__DIR__) . '/config'
+    )
+);
 
 $application = new Application('Runph', '[dev]');
 
--- src/Services/Config/ConfigLoader.php
+++ src/Services/Config/ConfigLoader.php
@@ -4,18 +4,23 @@ declare(strict_types=1);
 
 namespace Runph\Services\Config;
 
+use Runph\Services\Filesystem\Filesystem;
 use RuntimeException;
 
 class ConfigLoader
 {
     public function __construct(
+        private Filesystem $filesystem,
         private string $path,
     ) {
-        $realPath = realpath($path);
+        $fullpath = realpath($path);
 
-        if ($realPath === false || ! is_readable($realPath)) {
-            throw new RuntimeException("Config root path '{$path}' does not exist or is not readable");
+        if ($fullpath === false) {
+            $fullpath = $path;
         }
+
+        $this->filesystem->ensureReadable($fullpath);
+        $this->path = $fullpath;
     }
 
     /**
@@ -28,16 +33,7 @@ class ConfigLoader
         }
 
         $path = $this->path . DIRECTORY_SEPARATOR . $file;
-
-        if (! file_exists($path) || ! is_readable($path)) {
-            throw new RuntimeException("Config file '{$file}' not found in '{$this->path}'");
-        }
-
-        $loadFile = static function (string $fullpath) {
-            return require $fullpath;
-        };
-
-        $config = $loadFile($path);
+        $config = $this->filesystem->requireFile($path);
 
         if (! is_array($config)) {
             throw new RuntimeException("Config file '{$path}' must return an array, " . gettype($config) . ' returned');
