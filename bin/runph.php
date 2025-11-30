3c1cf434e5a48207815bab938743fb44787ca71f
Dreyf <cesardreyf@gmail.com>
2025-11-30 01:01:34 -0300

    Implementa CommandsAutoloader en la aplicación

--- bin/runph.php
+++ bin/runph.php
@@ -4,33 +4,22 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Runph\Services\CommandsAutoloader;
 use Runph\Services\Config\ConfigLoader;
 use Runph\Services\Container\Container;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 // Settings
 $container = new Container();
 $container->set(ContainerInterface::class, $container);
-
-/** @var ConfigLoader */
-$config = $container->set(ConfigLoader::class, new ConfigLoader(dirname(__DIR__) . '/config'));
+$container->set(ConfigLoader::class, new ConfigLoader(dirname(__DIR__) . '/config'));
 
 $application = new Application('Runph', '[dev]');
 
-/** @var string[] */
-$commands = $config->load('commands');
-
-foreach ($commands as $commandClassname) {
-    $command = $container->get($commandClassname);
-
-    if (! $command instanceof Command) {
-        throw new RuntimeException("The entry '{$commandClassname}' must be an instance of " . Command::class);
-    }
-
-    $application->addCommand($command);
-}
+/** @var CommandsAutoloader */
+$commandsAutoloader = $container->get(CommandsAutoloader::class);
+$commandsAutoloader->registerCommands($application);
 
 $application->run();
