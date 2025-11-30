c019e516c4c47459d0fa75b54eb408047e63a5c7
Dreyf <cesardreyf@gmail.com>
2025-11-29 23:48:48 -0300

    Implementa el contenedor de servicios en la aplicación

--- bin/runph.php
+++ bin/runph.php
@@ -3,9 +3,15 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
+use Runph\Services\Container\Container;
 use Symfony\Component\Console\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Settings
+$container = new Container();
+$container->set(ContainerInterface::class, $container);
 
 $application = new Application('Runph', '[dev]');
 
