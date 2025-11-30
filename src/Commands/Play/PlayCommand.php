1aeb7daa3643aac657512a45a9fa64ef2d7bf9ad
Dreyf <cesardreyf@gmail.com>
2025-11-30 00:19:38 -0300

    Crea el comando 'play'

---
+++ src/Commands/Play/PlayCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Runph\Commands\Play;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PlayCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('play')
+            ->setDescription('Ejecuta un playbook')
+            ->addArgument('file', InputArgument::REQUIRED, 'El archivo del playbook')
+            ->setHelp('Procesa y ejecuta un playbook');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Hola mundo');
+        return Command::SUCCESS;
+    }
+}
