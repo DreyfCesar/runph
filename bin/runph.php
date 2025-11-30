84340fed6d9b94a5bdc6d4fb9f98537d023876d4
Dreyf <cesardreyf@gmail.com>
2025-11-30 00:20:01 -0300

    Incluye el comando PlayCommand en la aplicación

--- bin/runph.php
+++ bin/runph.php
@@ -4,8 +4,10 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Runph\Commands\Play\PlayCommand;
 use Runph\Services\Container\Container;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -14,5 +16,19 @@ $container = new Container();
 $container->set(ContainerInterface::class, $container);
 
 $application = new Application('Runph', '[dev]');
+
+$defaultCommands = [
+    PlayCommand::class,
+];
+
+foreach ($defaultCommands as $commandClassname) {
+    $command = $container->get($commandClassname);
+
+    if (! $command instanceof Command) {
+        throw new RuntimeException("The entry '{$commandClassname}' must be an instance of " . Command::class);
+    }
+
+    $application->addCommand($command);
+}
 
 $application->run();
