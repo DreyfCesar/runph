c1540c006fd84d066817a053ff3a4efc17dbaeae
Dreyf <cesardreyf@gmail.com>
2025-12-04 22:44:09 -0300

    Agrega estilo para la impresión del título del playbook
    Modifica NameDirective para imprimir el título del playbook con estilos.

--- src/Playbook/Modules/Directives/NameDirective.php
+++ src/Playbook/Modules/Directives/NameDirective.php
@@ -6,16 +6,25 @@ namespace Runph\Playbook\Modules\Directives;
 
 use Runph\Playbook\Contracts\ModuleInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Terminal;
 
 class NameDirective implements ModuleInterface
 {
     public function __construct(
         private string $value,
         private OutputInterface $output,
+        private Terminal $terminal,
     ) {}
 
     public function run(): void
     {
-        $this->output->writeln($this->value);
+        $width = $this->terminal->getWidth();
+        $line = str_repeat('─', max(10, $width - 2));
+
+        $this->output->writeln('');
+        $this->output->writeln("<info>{$line}</info>");
+        $this->output->writeln("<comment> ▶  Playbook:</comment> <info>{$this->value}</info>");
+        $this->output->writeln("<info>{$line}</info>");
+        $this->output->writeln('');
     }
 }
