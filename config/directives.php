341b1ec8f67a5ff1480bd8fc9ad3732d7f779170
Dreyf <cesardreyf@gmail.com>
2025-12-05 21:29:30 -0300

    Agrega TasksDirective a la lista de directivas

--- config/directives.php
+++ config/directives.php
@@ -1,7 +1,9 @@
 <?php
 
 use Runph\Playbook\Modules\Directives\NameDirective;
+use Runph\Playbook\Modules\Directives\TasksDirective;
 
 return [
     'name' => NameDirective::class,
+    'tasks' => TasksDirective::class,
 ];
