f5b79016bb3d025571b2de41b2c1566b692b3937
Dreyf <cesardreyf@gmail.com>
2025-12-11 23:05:01 -0300

    Crea el archivo de configuración 'meta_handler'
    Este archivo contendrá la lista de gestores de metadatos.

---
+++ config/meta_handlers.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Runph\Playbook\Metadata\Handlers\NameHandler;
+use Runph\Playbook\Metadata\Handlers\WhenHandler;
+
+return [
+    NameHandler::class,
+    WhenHandler::class,
+];
