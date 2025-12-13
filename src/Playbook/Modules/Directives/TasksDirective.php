44f83b1a1cdc9aecebd8c9a050eae018fcd6037f
Dreyf <cesardreyf@gmail.com>
2025-12-12 22:02:09 -0300

    Implementa RegisterFactory en TasksDirective
    Más que nada es para las pruebas unitarias.

--- src/Playbook/Modules/Directives/TasksDirective.php
+++ src/Playbook/Modules/Directives/TasksDirective.php
@@ -6,22 +6,23 @@ namespace Runph\Playbook\Modules\Directives;
 
 use Runph\Playbook\Contracts\ModuleInterface;
 use Runph\Playbook\Metadata\MetaHandler;
-use Runph\Playbook\Metadata\Register;
+use Runph\Playbook\Metadata\RegisterFactory;
 
 class TasksDirective implements ModuleInterface
 {
     /**
-     * @param array<string, mixed>[] $value
+     * @param array<string, mixed>[] $value Tasks
      */
     public function __construct(
         private array $value,
         private MetaHandler $metaHandler,
+        private RegisterFactory $registerFactory,
     ) {}
 
     public function run(): void
     {
         foreach ($this->value as $id => $task) {
-            $register = new Register($task, $id);
+            $register = $this->registerFactory->make($task, $id);
 
             $this->metaHandler->run($register);
         }
