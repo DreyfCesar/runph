791bc4a5b147339e0d6678ac831da981b4d95218
Dreyf <cesardreyf@gmail.com>
2025-12-16 23:35:36 -0300

    Registra el evento TaskNameChanged y el listener TaskNamePrinterListener

--- config/listeners.php
+++ config/listeners.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use Runph\Playbook\Metadata\Events\TaskNameChanged;
+use Runph\Playbook\Presenters\Listeners\TaskNamePrinterListener;
+
 /** @var array<class-string<object>, string | list<class-string<object>>> */
 return [
+    TaskNameChanged::class => TaskNamePrinterListener::class,
 ];
