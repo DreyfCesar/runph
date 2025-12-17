f3e160ce1f41b53ef1e8959523d5900564a01dc7
Dreyf <cesardreyf@gmail.com>
2025-12-16 23:50:36 -0300

    Crea el trait StoppableEvent para eventos

---
+++ src/Events/Traits/StoppableEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Runph\Events\Traits;
+
+trait StoppableEvent
+{
+    private bool $propagationStopped = false;
+
+    public function isPropagationStopped(): bool
+    {
+        return $this->propagationStopped;
+    }
+
+    public function stopPropagation(): void
+    {
+        $this->propagationStopped = true;
+    }
+}
