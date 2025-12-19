c8815690edd11085840ec19c49193943a0645772
Dreyf <cesardreyf@gmail.com>
2025-12-18 21:56:59 -0300

    Agrega el trait StoppableEvent a TaskNameChanged
    No lo usa, pero para que deje de romper las b.... phpstan.

--- src/Playbook/Metadata/Events/TaskNameChanged.php
+++ src/Playbook/Metadata/Events/TaskNameChanged.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Runph\Playbook\Metadata\Events;
 
-class TaskNameChanged
+use Psr\EventDispatcher\StoppableEventInterface;
+use Runph\Events\Traits\StoppableEvent;
+
+class TaskNameChanged implements StoppableEventInterface
 {
+    use StoppableEvent;
+
     public function __construct(
         public readonly string $name,
     ) {}
