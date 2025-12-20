c855cc434e410287381aa2d0ed4eb77b5079eccf
Dreyf <cesardreyf@gmail.com>
2025-12-19 23:47:40 -0300

    Emplea SystemManager en runph.php
    Remueve todo el código del que ahora se encargan los sistemas y en su
    lugar se utiliza SystemManager para su ejecución.

--- bin/runph.php
+++ bin/runph.php
@@ -3,66 +3,23 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\EventDispatcher\ListenerProviderInterface;
-use Runph\Events\Dispatcher\SimpleEventDispatcher;
-use Runph\Events\Dispatcher\SimpleListenerProvider;
-use Runph\Playbook\Contracts\TaskPresenterInterface;
-use Runph\Playbook\Presenters\TaskPresenter;
-use Runph\Services\CommandsAutoloader;
-use Runph\Services\Config\ConfigLoader;
 use Runph\Services\Container\Container;
-use Runph\Services\Container\Contracts\FactoryContainerInterface;
 use Runph\Services\Container\ReflectionResolver;
-use Runph\Services\Filesystem\Filesystem;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\OutputInterface;
+use Runph\System\Application\ApplicationSystem;
+use Runph\System\Event\EventSystem;
+use Runph\System\Service\ServiceSystem;
+use Runph\System\SystemManager;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Settings
 $reflection = new ReflectionResolver();
 $container = new Container($reflection);
+$systems = new SystemManager($container);
 
 $reflection->setContainer($container);
-$container->set(ContainerInterface::class, $container);
-$container->set(FactoryContainerInterface::class, $container);
-$container->set(InputInterface::class, new ArgvInput());
-$container->set(OutputInterface::class, new ConsoleOutput());
-$container->set(TaskPresenterInterface::class, TaskPresenter::class);
 
-$listenerProvider = new SimpleListenerProvider($container);
-$eventDispatcher = new SimpleEventDispatcher($listenerProvider);
-$configLoader = new ConfigLoader($container->get(Filesystem::class), dirname(__DIR__) . '/config');
-
-$container->set(ListenerProviderInterface::class, $listenerProvider);
-$container->set(EventDispatcherInterface::class, $eventDispatcher);
-$container->set(ConfigLoader::class, $configLoader);
-
-/** @var array<class-string<object>, string|list<class-string<object>>> */
-$listenerList = $configLoader->load('listeners');
-
-foreach ($listenerList as $eventClass => $listeners) {
-    if (is_string($listeners)) {
-        $listeners = [$listeners];
-    }
-
-    foreach ($listeners as $listener) {
-        /** @var class-string<object> $listener */
-        $listenerProvider->addListener($eventClass, $listener);
-    }
-}
-
-$application = new Application('Runph', '[dev]');
-
-$commandsAutoloader = $container->get(CommandsAutoloader::class);
-$commandsAutoloader->registerCommands($application);
-
-$application->run(
-    input: $container->get(InputInterface::class),
-    output: $container->get(OutputInterface::class),
-);
+$systems->run([
+    new ServiceSystem(),
+    new EventSystem(),
+    new ApplicationSystem(),
+]);
