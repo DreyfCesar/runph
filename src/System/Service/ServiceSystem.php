90e02995ec37c07aff1f3bd43bb4716a1590fb81
Dreyf <cesardreyf@gmail.com>
2025-12-20 00:23:26 -0300

    Refactoriza ServiceSystem

--- src/System/Service/ServiceSystem.php
+++ src/System/Service/ServiceSystem.php
@@ -4,33 +4,32 @@ declare(strict_types=1);
 
 namespace Runph\System\Service;
 
-use Psr\Container\ContainerInterface;
-use Runph\Playbook\Contracts\TaskPresenterInterface;
-use Runph\Playbook\Presenters\TaskPresenter;
-use Runph\Services\Config\ConfigLoader;
 use Runph\Services\Container\Container;
-use Runph\Services\Container\Contracts\FactoryContainerInterface;
-use Runph\Services\Filesystem\Filesystem;
+use Runph\System\Service\Providers\ConfigServiceProvider;
+use Runph\System\Service\Providers\ConsoleServiceProvider;
+use Runph\System\Service\Providers\ContainerServiceProvider;
+use Runph\System\Service\Providers\PlaybookServiceProvider;
 use Runph\System\SystemInterface;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class ServiceSystem implements SystemInterface
 {
-    public function __construct(
-        private string $configPath,
-    ) {}
+    /** @var ServiceProviderInterface[] */
+    private array $providers;
+
+    public function __construct(string $configPath)
+    {
+        $this->providers = [
+            new ContainerServiceProvider(),
+            new ConsoleServiceProvider(),
+            new ConfigServiceProvider($configPath),
+            new PlaybookServiceProvider(),
+        ];
+    }
 
     public function execute(Container $container): void
     {
-        $container->set(ContainerInterface::class, $container);
-        $container->set(FactoryContainerInterface::class, $container);
-        $container->set(InputInterface::class, new ArgvInput());
-        $container->set(OutputInterface::class, new ConsoleOutput());
-        $container->set(ConfigLoader::class, new ConfigLoader($container->get(Filesystem::class), $this->configPath));
-
-        $container->set(TaskPresenterInterface::class, TaskPresenter::class);
+        foreach ($this->providers as $provider) {
+            $provider->register($container);
+        }
     }
 }
