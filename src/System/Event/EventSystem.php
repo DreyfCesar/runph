ba843d91254074f3d36a3e4dcae13fc0e5c55767
Dreyf <cesardreyf@gmail.com>
2025-12-19 23:22:42 -0300

    Crea EventSystem

---
+++ src/System/Event/EventSystem.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Runph\System\Event;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Runph\Events\Dispatcher\SimpleEventDispatcher;
+use Runph\Events\Dispatcher\SimpleListenerProvider;
+use Runph\Services\Config\ConfigLoader;
+use Runph\Services\Container\Container;
+use Runph\System\SystemInterface;
+
+class EventSystem implements SystemInterface
+{
+    public function execute(Container $container): void
+    {
+        $listenerProvider = new SimpleListenerProvider($container);
+        $eventDispatcher = new SimpleEventDispatcher($listenerProvider);
+        $configLoader = $container->get(ConfigLoader::class);
+
+        $container->set(ListenerProviderInterface::class, $listenerProvider);
+        $container->set(EventDispatcherInterface::class, $eventDispatcher);
+
+        /** @var array<class-string<object>, string|list<class-string<object>>> */
+        $listenerList = $configLoader->load('listeners');
+
+        foreach ($listenerList as $eventClass => $listeners) {
+            if (is_string($listeners)) {
+                $listeners = [$listeners];
+            }
+
+            foreach ($listeners as $listener) {
+                /** @var class-string<object> $listener */
+                $listenerProvider->addListener($eventClass, $listener);
+            }
+        }
+    }
+}
