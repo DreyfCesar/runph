526aa58c44d1296ecc44a40f9b267404a4e08e31
Dreyf <cesardreyf@gmail.com>
2025-12-20 00:23:13 -0300

    Crea PlaybookServiceProvider

---
+++ src/System/Service/Providers/PlaybookServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Runph\System\Service\Providers;
+
+use Runph\Playbook\Contracts\TaskPresenterInterface;
+use Runph\Playbook\Presenters\TaskPresenter;
+use Runph\Services\Container\Container;
+use Runph\System\Service\ServiceProviderInterface;
+
+class PlaybookServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $container): void
+    {
+        $container->set(TaskPresenterInterface::class, TaskPresenter::class);
+    }
+}
