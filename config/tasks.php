0ed35e24268e60e97cc337dfbea4ef801a1f2e79
Dreyf <cesardreyf@gmail.com>
2025-12-25 21:03:07 -0300

    Registra AskTask como tarea bajo la keyword 'ask'

--- config/tasks.php
+++ config/tasks.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use Runph\Playbook\Modules\Ask\AskTask;
 use Runph\Playbook\Modules\Log\LogTask;
 
 return [
     'log' => LogTask::class,
+    'ask' => AskTask::class,
 ];
