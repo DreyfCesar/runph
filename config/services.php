e376282770e0e38ab0bfaaa6ba4bfe1349c43607
Dreyf <cesardreyf@gmail.com>
2025-12-27 15:43:38 -0300

    Registra SharedMemory y lo asocia a MemoryInterface

--- config/services.php
+++ config/services.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Runph\Playbook\Contracts\TaskPresenterInterface;
 use Runph\Playbook\Presenters\TaskPresenter;
+use Runph\Services\Memory\Contracts\MemoryInterface;
+use Runph\Services\Memory\SharedMemory;
 
 return [
     TaskPresenterInterface::class => TaskPresenter::class,
+    MemoryInterface::class => SharedMemory::class,
 ];
